fb8dd36d6f3bc8949d0be47da1057aa2942ca94e
Douglas Moura <douglas.ademoura@gmail.com>
2023-10-04 22:15:34 -0300

    feat: cria sistema uma simples RPC API

---
+++ apps/exemplo-1/src/index.js
@@ -0,0 +1,39 @@
+import Fastify from 'fastify'
+import * as procedures from './procedures.js'
+
+const fastify = Fastify()
+
+fastify.post('/rpc', async (req, res) => {
+  if (!req.body?.procedure) {
+    res.status(400).send({
+      title: 'Faltam parâmetros obrigatórios',
+      details: 'O parâmetro "procedure" é obrigatório.'
+    })
+  }
+
+  const procedure = await procedures[req.body.procedure]
+
+  if (!procedure) {
+    return res.status(404).send({
+      title: 'Procedure não encontrada',
+      details: `A procedure ${req.body.procedure} não foi encontrada.`
+    })
+  }
+
+  try {
+    const response = procedure?.(req.body.args)
+    return res.status(response?.status || 200).send(response?.data)
+  } catch (error) {
+    return res.status(error?.status || 500).send({
+      title: error?.title || 'Erro',
+      details: error?.message || 'Ocorreu um erro.'
+    })
+  }
+})
+
+try {
+  await fastify.listen({ port: 3000 })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
---
+++ apps/exemplo-1/src/procedures.js
@@ -0,0 +1,101 @@
+class ProcedureError extends Error {
+  constructor(message, code, title) {
+    super(message)
+    this.title = title
+    this.code = code
+  }
+}
+
+let tarefas = [
+  {
+    id: 1,
+    title: 'Tarefa 1',
+  },
+  {
+    id: 2,
+    title: 'Tarefa 2',
+  },
+  {
+    id: 3,
+    title: 'Tarefa 3',
+  },
+]
+
+export const getTarefas = () => {
+  return {
+    status: 200,
+    data: tarefas,
+  }
+}
+
+export const getTarefa = (id) => {
+  if (!id) {
+    throw new ProcedureError('O id é obrigatório', 400, 'Faltam parâmetros obrigatórios')
+  }
+
+  const tarefa = tarefas.find((todo) => todo.id === id)
+
+  if (!tarefa) {
+    throw new ProcedureError(`Tarefa ${id} não encontrada`, 404)
+  }
+
+  return {
+    status: 200,
+    data: tarefa,
+  }
+}
+
+export const addTarefa = (title) => {
+  if (!title) {
+    throw new ProcedureError('O título é obrigatório', 400, 'Faltam parâmetros obrigatórios')
+  }
+
+  const id = tarefas.length + 1
+  const tarefa = {
+    id,
+    title,
+  }
+
+  tarefas.push(tarefa)
+
+  return {
+    status: 201,
+    data: { id },
+  }
+}
+
+export const updateTarefa = (id, title) => {
+  if (!id || !title) {
+    throw new ProcedureError('O id e o título são obrigatórios', 400, 'Faltam parâmetros obrigatórios')
+  }
+
+  const index = tarefas.findIndex((todo) => todo.id === id)
+
+  if (index === -1) {
+    throw new ProcedureError(`Tarefa ${id} não encontrada`, 404)
+  }
+
+  tarefas[index].title = title
+
+  return {
+    status: 200,
+  }
+}
+
+export const deleteTarefa = (id) => {
+  if (!id) {
+    throw new ProcedureError('O id é obrigatório', 400, 'Faltam parâmetros obrigatórios')
+  }
+
+  const index = tarefas.findIndex((todo) => todo.id === id)
+
+  if (index === -1) {
+    throw new ProcedureError(`Tarefa ${id} não encontrada`, 404)
+  }
+
+  tarefas = tarefas.filter((todo) => todo.id !== id)
+
+  return {
+    status: 204,
+  }
+}
